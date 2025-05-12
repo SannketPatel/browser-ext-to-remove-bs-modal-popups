fe8d7518e95a032c22d3cc66a6bde889abc80069
Sanket Patel <17946138+SannketPatel@users.noreply.github.com>
2025-05-12 10:09:35 +0530

    Update contentScript.js
    over flow hidden

--- contentScript.js
+++ contentScript.js
@@ -8,6 +8,7 @@ function removeModal() {
   modals.forEach(modal => {
     modal.remove();
   });
+  document.body.style= ""; // to remove overflow hidden kind of styles. Which blocks scrolling
 }
 
 // Remove modal after 2 seconds
